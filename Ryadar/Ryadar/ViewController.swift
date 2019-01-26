eee3cd603b86bfb1de83bb89b23d22d375288543
raja <raja.androiddeveloper@gmail.com>
2019-01-26 14:12:29 -0600

    init

---
+++ Ryadar/Ryadar/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Ryadar
+//
+//  Created by Rajashekar ryada on 1/26/19.
+//  Copyright © 2019 Rajashekar ryada. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
